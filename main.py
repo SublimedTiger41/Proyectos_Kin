886d3f6295c2975fc508e3cb39a8170e1fb3212c
SublimedTiger41 <jose.baca@uabc.edu.mx>
2024-11-05 12:43:05 -0800

    se agrego un hola mundo :3

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import pandas as pd
+
+def creardf():
+    df_mamalon = {"Amor_de_mi_vida":["Maria Fernananda Camarena Gonzales"],
+                  "Segundo_amor_de_mi_vida":["PCERDA"]}
+    return pd.DataFrame(df_mamalon)
+
 
 if __name__ == "__main__":
-    print("Hola mundo")
-    print("La mafer es hermosa")
+    df = creardf()
+    print(df)
+    #print("Hola mundo")
+    #print("La mafer es hermosa")
